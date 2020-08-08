208537832ec8997c7b73427699e3ef8465e92abf
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 21:34:40 +0300

    Project cleaning

---
+++ Assets/Scripts/DevelopConsole.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Sweet_And_Salty_Studios
+{
+	public class DevelopConsole : MonoBehaviour
+	{
+		#region VARIABLES
+
+		[SerializeField] private bool debug = default;
+		[SerializeField] private DevelopConsoleInputField developConsoleInputField = default;
+
+		#endregion VARIABLES
+
+		#region PROPERTIES
+
+        #endregion PROPERTIES
+
+        #region UNITY FUNCTIONS
+
+        #endregion UNITY FUNCTIONS
+
+        #region CUSTOM FUNCTIONS
+
+        public void OnValueChanged(string value)
+		{
+			// if(debug) Debug.Log($"OnValueChanged: {value}", gameObject);
+		}
+
+        public void OnEndEdit(string value)
+		{
+			//if(debug)Debug.Log($"OnEndEdit: {value}", gameObject);
+
+			developConsoleInputField.ChangeNewLine(value);
+		}
+
+		public void OnSelect(string value)
+		{
+			// if(debug) Debug.Log($"OnSelect: {value}", gameObject);
+		}
+
+		public void OnDeselect(string value)
+		{
+			// if(debug) Debug.Log($"OnDeselect: {value}", gameObject);
+		}
+
+		#endregion CUSTOM FUNCTIONS	
+	}
+}
---
+++ Assets/Scripts/DevelopConsoleInputField.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Sweet_And_Salty_Studios
+{
+	public class DevelopConsoleInputField : TMP_InputField
+	{
+        #region VARIABLES
+
+        [SerializeField] private DevelopConsole developConsole;
+        
+        #endregion VARIABLES
+
+        #region PROPERTIES
+
+        #endregion PROPERTIES
+
+        #region UNITY FUNCTIONS
+
+        #endregion UNITY FUNCTIONS
+
+        #region CUSTOM FUNCTIONS
+
+        public override void OnSubmit(BaseEventData eventData)
+        {
+            base.OnSubmit(eventData);
+
+            Debug.Log($"OnSubmit", gameObject);
+        }
+
+        public void ChangeNewLine(string value)
+        {
+            text = $"{value}\n";
+            caretPosition = value.Length + 1;
+            ActivateInputField();
+        }
+
+        #endregion CUSTOM FUNCTIONS	
+    }
+}
